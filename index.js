6f37e9edc1df5038c9a6169e503ce6b334110f4c
Rytis Černiauskas <rytis.cerniauskas@gmail.com>
2024-01-24 19:38:42 +0200

    Fixed index.js file

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+const express = require("express"); //importuojam express
+const app = express(); //inicijuojam express aplikacija
+const port = 3001; //uostas
+
+//sukuriam route(kelia) kuriuo uzejus grazinsime (GET metodas) teksta "Hello, world"
+app.get("/", (req, res) => {
+  //request/response
+  res.send("Hello, word"); //issiuncia kvietejui atsakyma
+});
+const cars = ["Bmw", "Audi", "WV"];
+app.get("/cars", (req, res) => {
+  res.send(cars);
+});
+
+const students = [{ id: 1, name: "Rytis", age: 26 }];
+app.get("/students", (req, res) => {
+  res.send(students);
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on the port ${port}`);
+});
