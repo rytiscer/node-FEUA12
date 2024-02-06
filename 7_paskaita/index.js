4fdb37fb572ccd04a9c4c3896f4803c425cf3b5e
rytiscer <rytis.cerniauskas@gmail.com>
2024-02-06 20:22:04 +0200

    added 7_paskaita folder with other files and code

---
+++ 7_paskaita/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 8080;
+const URI =
+  "mongodb+srv://admin:admin@cluster0.jdz8luo.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(URI);
+
+app.get("/pets", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db("pets").collection("pets").find().toArray();
+    await con.close();
+    return res.send(data);
+  } catch (err) {
+    console.error("Error fetching data from MongoDB:", err);
+    res.status(500).send({ error: "Failed to fetch data from MongoDB" });
+  }
+});
+
+app.post("/pets", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const dbRes = await con.db("pets").collection("pets").insertOne({
+      name: "Bob",
+      type: "mouse",
+      age: 5,
+    });
+    await con.close();
+    return res.send(dbRes);
+  } catch (err) {
+    res.status(500).send({ err });
+  }
+});
+
+client.connect((err) => {
+  if (err) {
+    console.error("Failed to connect to MongoDB:", err);
+    return;
+  }
+  console.log("Connected to MongoDB");
+
+  // Start the Express server after successful connection
+  app.listen(port, () => {
+    console.log(`Server is running on ${port} port`);
+  });
+});
