0de4c3a0ad3364ae09ae5e2a30e9a2d65ca760dd
rytiscer <rytis.cerniauskas@gmail.com>
2024-02-19 20:24:22 +0200

    Added all routes

--- 9_paskaita/index.js
+++ 9_paskaita/index.js
@@ -53,6 +53,49 @@ app.delete("/memberships/:id", async (req, res) => {
   }
 });
 
+app.get("/users", async (req, res) => {
+  const order = req.params.order;
+  try {
+    const con = await client.connect();
+    const data = await con
+      .db("demo1")
+      .collection("users2")
+      .aggregate([
+        {
+          $addFields: {
+            service_id: { $toObjectId: "$service_id" }, // Convert service_id to ObjectId
+          },
+        },
+        {
+          $lookup: {
+            from: "services",
+            localField: "service_id",
+            foreignField: "_id",
+            as: "Membership",
+          },
+        },
+      ])
+      .toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).send({ error });
+  }
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const newUser = req.body;
+    const con = await client.connect();
+    const dbRes = await con.db("demo1").collection("users2").insertOne(newUser);
+    await con.close();
+    res.send(dbRes);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on ${port} port`);
 });
