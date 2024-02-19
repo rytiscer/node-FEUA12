f6d4af195a27cf2b0da67f201d505aa95f9a2a66
rytiscer <rytis.cerniauskas@gmail.com>
2024-02-19 19:52:05 +0200

    Added membership routes

--- 9_paskaita/index.js
+++ 9_paskaita/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -8,7 +8,50 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 8080;
-const uri = process.env.DB_CONNECTION_STRING;
+const URI = process.env.DB_CONNECTION_STRING;
+
+const client = new MongoClient(URI);
+
+app.get("/memberships", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db("demo1").collection("services").find().toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.post("/memberships", async (req, res) => {
+  try {
+    const newService = req.body;
+    const con = await client.connect();
+    const dbRes = await con
+      .db("demo1")
+      .collection("services")
+      .insertOne(newService);
+    await con.close();
+    res.send(dbRes);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.delete("/memberships/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const con = await client.connect();
+    const data = await con
+      .db("demo1")
+      .collection("services")
+      .deleteOne({ _id: new ObjectId(id) });
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port} port`);
